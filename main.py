afdd95dd5615e3596d4024e325d38d836aec7123
Jestyr22 <sebastianvoorhees1997@gmail.com>
2025-01-31 02:47:59 +0000

    Adding player file, and making the player appear on screen

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     Clock = pygame.time.Clock()
-    dt = 0
+    dt = 0 #Delta Time = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return #Makes the windows Close button correctly exit the game
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        Clock.tick(60)
-        dt = (Clock.tick(60) / 1000)
+        #Clock.tick(60) Might not be needed, instructions said to do it in two lines but I think I can do it all in one?
+        dt = (Clock.tick(60) / 1000) #Limit frames to 60FPS, assigns DT the value in seconds (Hence the / 1000, otherwise it would be milliseconds)
+
 
 
 

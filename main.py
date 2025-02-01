13c694f53326c5b9b0cf9f8f83c1a30585b692bf
Jestyr22 <sebastianvoorhees1997@gmail.com>
2025-02-01 04:30:32 +0000

    Removing/Splitting asteroids when shot

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.split()
+                    shot.kill()
+
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
